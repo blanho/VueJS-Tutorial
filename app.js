b951df061389ad49258bc4d1027a68d603b73508
blanho <h.baolan20025@gmail.com>
2023-04-04 06:08:57 +0700

    feat: computed properties

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "http://www.thenetninja.com",
       showBooks: true,
-      title: "The final empire",
-      author: "Brandon Sanderson",
-      age: 45,
-      x: 0,
-      y: 0,
+      books: [
+        { title: "name of the wind", author: "patrick rothfuss" },
+        { title: "name of the wind", author: "patrick rothfuss" },
+        { title: "name of the wind", author: "patrick rothfuss" },
+        { title: "name of the wind", author: "patrick rothfuss" },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
     },
-    handleEvent(e, data) {
-      console.log(e, e.type);
-      if (data) {
-        console.log(data);
-      }
-    },
-    handleMousemove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.title === "name of the wind");
     },
   },
 });
